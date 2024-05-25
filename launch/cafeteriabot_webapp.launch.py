f282adefa0b4cb990fc670ff256f8ae940c1e211
Abhishek Verma <abhishek_verma@hotmail.com>
2024-05-25 04:06:51 +0000

    fix: updated to include rosbridge launch

--- launch/cafeteriabot_webapp.launch.py
+++ launch/cafeteriabot_webapp.launch.py
@@ -5,28 +5,35 @@ from launch_ros.actions import Node
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import ExecuteProcess
+from launch.actions import IncludeLaunchDescription
 from launch.actions import TimerAction
 from launch.substitutions import LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
 from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import AnyLaunchDescriptionSource
 
 
 def generate_launch_description():
     # launch parameters
     use_sim_time = LaunchConfiguration("use_sim_time", default="True")
+    use_rosbridge = LaunchConfiguration("use_rosbridge", default="True")
 
     # package cafeteriabot_webapp
     path_root = Path(get_package_share_directory("cafeteriabot_webapp"))
+
+    # package rosbridge_server
+    path_web2 = Path(get_package_share_directory("rosbridge_server"))
 
     # return launch
     return LaunchDescription(
         [
             DeclareLaunchArgument(name="use_sim_time", default_value="True"),
-            Node(
-                package="rosbridge_server",
-                executable="rosbridge_websocket",
-                name="rosbridge_websocket",
-                output="screen",
+            DeclareLaunchArgument(name="use_rosbridge", default_value="True"),
+            IncludeLaunchDescription(
+                AnyLaunchDescriptionSource(
+                    (path_web2 / "launch" / "rosbridge_websocket_launch.xml").as_posix()
+                ),
+                condition=IfCondition(LaunchConfiguration('use_rosbridge')),
             ),
             ExecuteProcess(
                 cmd=["python3", "-m", "http.server", "7000"],
